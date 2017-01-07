e071139b8c502dfe060b3008b6eaa9d79e3d98b0
webenot <webenot@mail.ua>
2017-01-07 19:02:28 +0200

    Вебинар 'Just prototype this'  - 2-1

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /**
  * Created by aquilla on 07.01.2017.
  */
+/*
 function summ (a, b) {
     //console.log(this);
     return this.prop + a + b;
@@ -19,38 +20,38 @@ let a = {
         prop: 2,
         f: summ
     };
-/*
+/!*
 a.f();
 b.f();
 var newFunc = a.f.bind(b);
 
-newFunc();*/
+newFunc();*!/
 
-/*a.f = a.f.bind(b);
+/!*a.f = a.f.bind(b);
 
 a.f();
-b.f();*/
+b.f();*!/
 
-/*a.f(1, 1);
+/!*a.f(1, 1);
 b.f(2, 2);
 
 a.f = a.f.bind(b, 1, 1);
 b.f = b.f.bind(a, 2, 2);
 
 a.f();
-b.f();*/
+b.f();*!/
 
-/*console.log(summ.apply(a, [1, 1]));
-console.log(summ.apply(b, [2, 2]));*/
+/!*console.log(summ.apply(a, [1, 1]));
+console.log(summ.apply(b, [2, 2]));*!/
 
-/*
+/!*
 summ(1, 1);
-*/
+*!/
 
-/*
+/!*
 a.f(1, 1);
 // Равносильно
-a['f'](1, 1);*/
+a['f'](1, 1);*!/
 
 //a.f();
 
@@ -59,4 +60,10 @@ let newFunc = summ.bind(a, 1);
 //newFunc = newFunc.bind(a, 1);
 //console.log(newFunc(2));
 
-setTimeout(a.f.bind(a), 1000);
+setTimeout(a.f.bind(a), 1000);*/
+
+let F = function () {
+};
+
+let obj = new F();
+console.log(obj);
