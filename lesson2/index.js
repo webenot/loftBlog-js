9d410aa793fba23aa441b429f4ab3aedab618875
webenot <webenot@mail.ua>
2017-01-08 14:25:55 +0200

    Урок 2 - функции 1

--- lesson2/index.js
+++ lesson2/index.js
@@ -1,3 +1,14 @@
 /**
  * Created by aquilla on 08.01.2017.
  */
+function summ(source) {
+	let result = 0;
+	for(let i = 0; i < source.length; i++) {
+		result += source[i];
+	}
+	return result;
+}
+
+let array = [1,2,3];
+
+console.log(summ(array));
