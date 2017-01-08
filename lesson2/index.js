7443544b7bc6c2c75dbb5ffcd487e60597b2465d
webenot <webenot@mail.ua>
2017-01-08 14:54:33 +0200

    Урок 2 - функции 5

--- lesson2/index.js
+++ lesson2/index.js
@@ -1,13 +1,13 @@
 /**
  * Created by aquilla on 08.01.2017.
  */
-function sum() {
+let sum = function() {
 	let result = 0;
 	for(let i = 0; i < arguments.length; i++) {
 		result += arguments[i];
 	}
 	return result;
-}
+};
 
 //let array = [1,2,3];
 
