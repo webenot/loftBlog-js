634a64ab8d7990413de2c92a9b32d5889a178bf0
webenot <webenot@mail.ua>
2017-01-08 15:05:39 +0200

    Урок 2 - функции 6

--- lesson2/index.js
+++ lesson2/index.js
@@ -1,6 +1,7 @@
 /**
  * Created by aquilla on 08.01.2017.
  */
+/*
 let sum = function() {
 	let result = 0;
 	for(let i = 0; i < arguments.length; i++) {
@@ -15,4 +16,20 @@ let sum2 = sum;
 
 let result = sum2(1,2,3);
 
-console.log(result);
+console.log(result);*/
+
+function filter(source) {
+	let result = [];
+	for (let i = 0; i< source.length; i++) {
+		if (source[i] > 4) {
+			result.push(source[i]);
+		}
+	}
+	return result;
+}
+
+let array = [1,2,3,4,5,6,7,8,9];
+
+let res = filter(array);
+
+console.log(res);
