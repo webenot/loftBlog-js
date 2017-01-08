c492d1a4531dd0471c50475b52ea1207bc53a201
webenot <webenot@mail.ua>
2017-01-08 15:34:34 +0200

    Урок 2 - функции 9

--- lesson2/index.js
+++ lesson2/index.js
@@ -18,7 +18,7 @@ let result = sum2(1,2,3);
 
 console.log(result);*/
 
-function filter(source, fn) {
+/*unction filter(source, fn) {
 	let result = [];
 	for (let i = 0; i< source.length; i++) {
 		if (fn(source[i])) {
@@ -38,4 +38,16 @@ let res = filter(array, function (value) {
 	return value > 4;
 });
 
-console.log(res);
+console.log(res);*/
+
+function func1() {
+	let func2 = function () {
+		return 'Привет, Мир!';
+	};
+
+	return func2;
+}
+
+let res = func1();
+console.log(res());
+//console.log(res2);
