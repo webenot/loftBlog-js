c5e6d2c464ab1ed7a542daaaca72c641fa18b876
webenot <webenot@mail.ua>
2017-01-08 14:46:23 +0200

    Урок 2 - функции 3 аргументы

--- lesson2/index.js
+++ lesson2/index.js
@@ -1,16 +1,16 @@
 /**
  * Created by aquilla on 08.01.2017.
  */
-function sum(source) {
+function sum() {
 	let result = 0;
-	for(let i = 0; i < source.length; i++) {
-		result += source[i];
+	for(let i = 0; i < arguments.length; i++) {
+		result += arguments[i];
 	}
 	return result;
 }
 
-let array = [1,2,3];
+//let array = [1,2,3];
 
-let result = sum(array);
+let result = sum(1,2,3);
 
 console.log(result);
