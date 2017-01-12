cd6a75ebc46ee4d0b6a886320457a57078899378
webenot <webenot@mail.ua>
2017-01-12 16:45:08 +0200

    Урок 3 - обработка ошибок и отладка 4 - finally

--- lesson3/exception.js
+++ lesson3/exception.js
@@ -2,22 +2,22 @@
  * Created by aquilla on 11.01.2017.
  */
 function div(a, b) {
-	if (b == 0) {
-		throw new Error('На ноль делить нельзя!');
-	}
-	if (b < 0) {
-		throw new Error('Делитель должен быть положительным числом!');
+	try {
+		if (b == 0) {
+			throw new Error('На ноль делить нельзя!');
+		}
+		if (b < 0) {
+			throw new Error('Делитель должен быть положительным числом!');
 
+		}
+		return a / b;
+	} catch (e) {
+		console.error(e.message);
+		return 0;
+	} finally {
+		console.log('Операция завершена!');
 	}
-	return a / b;
 }
 
-try {
-	let res = div(10, '0');
-	console.log(res + 1000);
-} catch (e) {
-	console.error(e.message);
-} finally {
-	console.log('Finally!');
-}
-console.log('!!!');
+let res = div(10, '0');
+console.log(res + 1000);
