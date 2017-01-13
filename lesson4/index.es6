0d38e8fd4d886f13da4cd6171e92436af6096534
webenot <webenot@mail.ua>
2017-01-13 18:32:01 +0200

    Урок 4 - структуры данных - обьекты 1

---
+++ lesson4/index.es6
@@ -0,0 +1,8 @@
+let obj1 = {
+	name: 'Сергей',
+	lastName: 'Мелюков'
+};
+
+console.log(obj1.lastName);
+console.log(obj1['lastName']);
+console.log(obj1[1]); // нельзя - будет undefined
---
+++ lesson4/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var obj1 = {
+	name: 'Сергей',
+	lastName: 'Мелюков'
+};
+
+console.log(obj1.lastName);
+console.log(obj1['lastName']);
+console.log(obj1[1]); // нельзя - будет undefined
+
+//# sourceMappingURL=index.js.map
