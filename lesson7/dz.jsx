260e9136faf8eadeee357f6d86cbf5e94e665670
webenot <webenot@mail.ua>
2017-02-14 16:04:21 +0200

    Урок 7 - Events - home work  2

--- lesson7/dz.jsx
+++ lesson7/dz.jsx
@@ -11,3 +11,87 @@ let openAccordeon = (e) => {
 };
 
 accordeon.addEventListener('click', openAccordeon);
+
+let createBlock = () => {
+	let c = document.querySelector('.right-block');
+	c.innerHTML = '';
+	let newDiv = document.createElement('div');
+	let r = Math.floor(Math.random() * (256)),
+		g = Math.floor(Math.random() * (256)),
+		b = Math.floor(Math.random() * (256)),
+		width = Math.floor(Math.random() * (c.clientWidth / 2)),
+		height = Math.floor(Math.random() * (c.clientHeight / 2)),
+		x = Math.floor(Math.random() * (c.clientWidth - width)),
+		y = Math.floor(Math.random() * (c.clientHeight - height));
+
+	newDiv.classList.add('draggable');
+	newDiv.style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
+	newDiv.style.position = 'absolute';
+	newDiv.style.width = width + 'px';
+	newDiv.style.height = height + 'px';
+	newDiv.style.top = y + 'px';
+	newDiv.style.left = x + 'px';
+	newDiv.addEventListener('mouseover', (e) => {
+		e.target.style.cursor = 'move';
+	});
+	newDiv.addEventListener('mouseout', (e) => {
+		e.target.style.cursor = 'default';
+	});
+	c.appendChild(newDiv);
+};
+
+let btn = document.querySelector('#createBlock');
+btn.addEventListener('click', createBlock);
+
+let container = document.querySelector('.right-block');
+let cont = container.getBoundingClientRect();
+let movableElement, shiftX = 0, shiftY = 0;
+
+let getDiv = (e) => {
+	console.log('Нажали кнопку мыши', e);
+	if (e.which != 1) { // если клик правой кнопкой мыши
+		return; // то он не запускает перенос
+	}
+	movableElement = e.target.closest('.draggable');
+
+	if (!movableElement) return;
+	let box = movableElement.getBoundingClientRect();
+
+	shiftX = e.clientX - box.left;
+	shiftY = e.clientY - box.top;
+	console.log(shiftX, shiftY);
+};
+let leaveDiv = (e) => {
+	console.log('Отпустили кнопку мыши', e);
+	/*movableElement.style.top = e.clientY - shiftY - cont.top + 'px';
+	movableElement.style.left = e.clientX - shiftX - cont.left + 'px';*/
+	movableElement = null;
+};
+let moveDiv = (e) => {
+	if (movableElement) {
+		if (e.clientY - shiftY  - cont.top < cont.top) {
+			movableElement.style.top = '0';
+		} else {
+			if (e.clientY - shiftY > cont.top + cont.height - movableElement.clientHeight) {
+				movableElement.style.top = cont.top + cont.height - movableElement.clientHeight + 'px';
+			} else {
+				movableElement.style.top = e.clientY - shiftY  - cont.top + 'px';
+			}
+		}
+		if (e.clientX - shiftX < cont.left) {
+			movableElement.style.left = '0';
+		} else {
+			if (e.clientX - shiftX > cont.left + cont.width - movableElement.clientWidth) {
+				console.log(cont.left + cont.width - movableElement.clientWidth);
+				movableElement.style.left = cont.width - movableElement.clientWidth + 'px';
+			} else {
+				movableElement.style.left = e.clientX - shiftX - cont.left + 'px';
+			}
+		}
+		//console.dir(e.currentTarget);
+	}
+};
+
+container.addEventListener('mousedown', getDiv);
+container.addEventListener('mouseup', leaveDiv);
+container.addEventListener('mousemove', moveDiv);
--- lesson7/dz.out.js
+++ lesson7/dz.out.js
@@ -42,6 +42,93 @@ var openAccordeon = function openAccordeon(e) {
 
 accordeon.addEventListener('click', openAccordeon);
 
+var createBlock = function createBlock() {
+	var c = document.querySelector('.right-block');
+	c.innerHTML = '';
+	var newDiv = document.createElement('div');
+	var r = Math.floor(Math.random() * 256),
+	    g = Math.floor(Math.random() * 256),
+	    b = Math.floor(Math.random() * 256),
+	    width = Math.floor(Math.random() * (c.clientWidth / 2)),
+	    height = Math.floor(Math.random() * (c.clientHeight / 2)),
+	    x = Math.floor(Math.random() * (c.clientWidth - width)),
+	    y = Math.floor(Math.random() * (c.clientHeight - height));
+
+	newDiv.classList.add('draggable');
+	newDiv.style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
+	newDiv.style.position = 'absolute';
+	newDiv.style.width = width + 'px';
+	newDiv.style.height = height + 'px';
+	newDiv.style.top = y + 'px';
+	newDiv.style.left = x + 'px';
+	newDiv.addEventListener('mouseover', function (e) {
+		e.target.style.cursor = 'move';
+	});
+	newDiv.addEventListener('mouseout', function (e) {
+		e.target.style.cursor = 'default';
+	});
+	c.appendChild(newDiv);
+};
+
+var btn = document.querySelector('#createBlock');
+btn.addEventListener('click', createBlock);
+
+var container = document.querySelector('.right-block');
+var cont = container.getBoundingClientRect();
+var movableElement = void 0,
+    shiftX = 0,
+    shiftY = 0;
+
+var getDiv = function getDiv(e) {
+	console.log('Нажали кнопку мыши', e);
+	if (e.which != 1) {
+		// если клик правой кнопкой мыши
+		return; // то он не запускает перенос
+	}
+	movableElement = e.target.closest('.draggable');
+
+	if (!movableElement) return;
+	var box = movableElement.getBoundingClientRect();
+
+	shiftX = e.clientX - box.left;
+	shiftY = e.clientY - box.top;
+	console.log(shiftX, shiftY);
+};
+var leaveDiv = function leaveDiv(e) {
+	console.log('Отпустили кнопку мыши', e);
+	/*movableElement.style.top = e.clientY - shiftY - cont.top + 'px';
+ movableElement.style.left = e.clientX - shiftX - cont.left + 'px';*/
+	movableElement = null;
+};
+var moveDiv = function moveDiv(e) {
+	if (movableElement) {
+		if (e.clientY - shiftY - cont.top < cont.top) {
+			movableElement.style.top = '0';
+		} else {
+			if (e.clientY - shiftY > cont.top + cont.height - movableElement.clientHeight) {
+				movableElement.style.top = cont.top + cont.height - movableElement.clientHeight + 'px';
+			} else {
+				movableElement.style.top = e.clientY - shiftY - cont.top + 'px';
+			}
+		}
+		if (e.clientX - shiftX < cont.left) {
+			movableElement.style.left = '0';
+		} else {
+			if (e.clientX - shiftX > cont.left + cont.width - movableElement.clientWidth) {
+				console.log(cont.left + cont.width - movableElement.clientWidth);
+				movableElement.style.left = cont.width - movableElement.clientWidth + 'px';
+			} else {
+				movableElement.style.left = e.clientX - shiftX - cont.left + 'px';
+			}
+		}
+		//console.dir(e.currentTarget);
+	}
+};
+
+container.addEventListener('mousedown', getDiv);
+container.addEventListener('mouseup', leaveDiv);
+container.addEventListener('mousemove', moveDiv);
+
 
 },{"babel-runtime/core-js/get-iterator":2}],2:[function(require,module,exports){
 module.exports = { "default": require("core-js/library/fn/get-iterator"), __esModule: true };
